cdcb94a66e63faa4e76143e8a96a33455ecb8c7c
Jovian <ajovian440@gmail.com>
2025-02-26 14:45:21 +1000

    [Fix] [Green] Add signup/signin button, make Navbar responnsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useTheme } from '../context/ThemeContext';
 import logo from '../assets/images/logo.png'
 import styles from '../styles/Navbar.module.css';
+import { Button } from './Button';
 
 export const Navbar = () => {
   const { theme, toggleTheme } = useTheme();
@@ -10,19 +11,31 @@ export const Navbar = () => {
     <nav className={styles.navbar}>
       <div className={styles.logoContainer}>
         <Link to="/">
-          <div className={styles.logoInnerContainer}>
+          <div className={styles.flexCenterContainer}>
             <img src={logo} alt="Data Talk Logo" className={styles.logo} />
             <span className={styles.logoText}>Data Talk</span>
           </div>
         </Link>
       </div>
-      <button
-        onClick={toggleTheme}
-        className={styles.themeButton}
-        aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
-      >
-        {theme === 'light' ? '🌙' : '☀️'}
-      </button>
+      <div className={styles.buttonContainer}>
+        <Button 
+          label='Sign In'
+          onClick={() => {}}
+          className={`${styles.navbarButton} ${styles.signInButton}`}
+          />
+        <Button 
+          label='Sign Up'
+          onClick={() => {}}
+          className={`${styles.navbarButton} ${styles.signUpButton}`}
+        />
+        <button
+          onClick={toggleTheme}
+          className={`${styles.navbarButton} ${styles.themeButton}`}
+          aria-label={`Switch to ${theme === 'light' ? 'dark' : 'light'} mode`}
+        >
+          {theme === 'light' ? '🌙' : '☀️'}
+        </button>
+      </div>
     </nav>
   );
 };
